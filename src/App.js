dfaee0145b3e424cd97e55a49449356c1f76f6ab
Rohan Marya <rohanmarya1507@gmail.com>
2024-01-16 23:56:43 +0530

    Using context api

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
-
+import Notestate from './context/notes/Notestate';
 
 function App() {
   return (
     <>
+    <Notestate>
       <Router>
           <Navbar/>
           <Routes>
@@ -15,6 +16,7 @@ function App() {
               <Route exact path="/about"  element={<About />}></Route>
           </Routes>
         </Router>
+    </Notestate>
     </>
   );
 }
