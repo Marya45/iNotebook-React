d1639f303897859cb49238655346042f09c91cf2
Rohan Marya <rohanmarya1507@gmail.com>
2024-01-17 00:39:45 +0530

    Using useLocation Hook and adding a basic structure of form in home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Notestate from './context/notes/Notestate';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Notestate from "./context/notes/Notestate";
 
 function App() {
   return (
     <>
-    <Notestate>
-      <Router>
-          <Navbar/>
-          <Routes>
-              <Route exact path="/"  element={<Home />}></Route>
-              <Route exact path="/about"  element={<About />}></Route>
-          </Routes>
+      <Notestate>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route exact path="/about" element={<About />}></Route>
+            </Routes>
+          </div>
         </Router>
-    </Notestate>
+      </Notestate>
     </>
   );
 }
--- src/context/notes/Notestate.js
+++ src/context/notes/Notestate.js
@@ -3,21 +3,9 @@ import NoteContext from './noteContext';
 
 //it will provide all state of note
 const NoteState = (props)=>{
-    const s1 = {
-        "name" : "Rohan",
-        "class" : "5d"
-    }
-    const [state,setState] = useState(s1);
-    const update = ()=>{
-        setTimeout(() => {
-            setState({
-                "name" : "King",
-                "class" : "10d"
-            });
-        }, 1000);
-    }
+    
     return(
-        <NoteContext.Provider value={{state,update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
