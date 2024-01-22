ebb3cc31a6a34a3039ce5752c4836aaf96f8ceea
Rohan Marya <rohanmarya1507@gmail.com>
2024-01-22 23:32:45 +0530

    Added deletenote functionality

--- src/context/notes/Notestate.js
+++ src/context/notes/Notestate.js
@@ -81,8 +81,11 @@ const NoteState = (props)=>{
     }
     
     // Delete a note
-    const deleteNote = ()=>{
+    const deleteNote = (id)=>{
+      console.log("Deleting node with id "+id);
 
+      const newNotes = notes.filter((note)=>{return note._id!==id});
+      setNotes(newNotes);
     }
     
     // Edit a note
